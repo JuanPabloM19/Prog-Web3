8a07721ec1b2d8e23b361840242c59c644f35750
JPMunizaga <jpmunizaga19gmail.com>
2024-08-21 19:18:40 -0300

    Configuracion de Java,Apache,NetBeans y agregacion de Servlet

---
+++ src/java/ServletClase2.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Juan Pablo
+ */
+@WebServlet(urlPatterns = {"/ServletClase2"})
+/* public class ServletClase2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   /* protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+        /*    out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletClase2</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ServletClase2 at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    /* @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   /* @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+   /* @Override
+    /* public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+} */
+
+public class ServletClase2 extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String titulo = "Su Información";
+        
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<body bgcolor=\"#fdf5e6\">\n");
+            out.println("<h1 align=center>" + titulo + "</h1>\n");
+            out.println("<ul>\n");
+            out.println(" <li><b>Nombre</b>: " + request.getParameter("nombre") + "\n");
+            out.println(" <li><b>Correo</b>: " + request.getParameter("correo") + "\n");
+            out.println(" <li><b>Password</b>: " + request.getParameter("password") + "\n");
+            out.println("</ul>\n");
+            out.println("</body></html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
